e83d5e99506d25a4f6dbbcab99e7237f3690c477
Rich Hodgkins <rich@bookcreator.com>
2026-09-02 11:47:21 +0100

    Moved to raw Node types and extracted separate sub-module for express types

---
+++ express.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="express-serve-static-core" preserve="true" />
+import type { Request, Response } from 'express-serve-static-core';
+import type { Logging as _Logging, StructuredRequestLogger } from './';
+
+// This only needs to export types as the backing functionality is the same
+// Also allow defined properties to be accessed if needed
+export class Logging<ReqProps = {}> extends _Logging<Request & ReqProps, Response> { }
+
+// Add in support for a .log property on an Express request
+declare global {
+   namespace Express {
+      interface Request {
+         readonly log: StructuredRequestLogger;
+      }
+   }
+}
